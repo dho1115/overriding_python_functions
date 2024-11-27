e889960a905faa9d132914bb8b5f355b0345a60c
admin_dho <davidhorph@hotmail.com>
2024-11-27 00:08:31 -0600

    FINALLY FIGURED OUT HOW TO MAKE PRINT... PRINTgit status! IT'S MIDNIGHT. I'M TIRED.

---
+++ OverrideFolder/fileone.py
@@ -0,0 +1,3 @@
+def myprint(args, end='\n'):
+   return print(f"This print is imported from file one. Here is the message: {args}.", end=end)
+
---
+++ OverrideFolder/main.py
@@ -0,0 +1,6 @@
+from fileone import myprint;
+
+def print(args, end="\n"):
+   return myprint(args, end=end);
+
+print("Hello, world... how are you?", end=" ----- ");
